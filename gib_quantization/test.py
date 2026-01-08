7743d8a8e4336f4bd34ae66a8375b1e81a66da21
frbinucci <binucci.francesco@gmail.com>
2026-01-08 17:43:35 +0100

    Fix plotting issues

--- gib_quantization/test.py
+++ gib_quantization/test.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 import argparse
 from sklearn.linear_model import LinearRegression
 
@@ -11,6 +13,17 @@ import matplotlib.pyplot as plt
 
 from regression_test import nmse_db
 
+
+from matplotlib import rcParams
+
+
+def set_matplotlib_style(font_size: int = 14) -> None:
+    """Set consistent matplotlib styling for paper-quality figures."""
+    rcParams["mathtext.fontset"] = "stix"
+    rcParams["font.family"] = "STIXGeneral"
+    rcParams["font.size"] = font_size
+    rcParams["legend.fontsize"] = "medium"
+    rcParams["axes.grid"] = True
 
 def compute_nmse(Y,Y_hat):
     nmse = 0
@@ -37,6 +50,7 @@ def main():
     parser.add_argument("--nx", type=int, default=50)
     parser.add_argument("--ny", type=int, default=35)
     parser.add_argument("--use_closed_form", action="store_true")  # se vuoi anche OLS
+    parser.add_argument("--quantization_bits",type=int,default=32)
     args = parser.parse_args()
 
     training_data, validation_data, training_loader, validation_loader, _ = \
@@ -49,9 +63,9 @@ def main():
     nmse_db_array = np.zeros(len(n_components_array))
     nmse_quantization_array = np.zeros(len(n_components_array))
 
-    component_index = 1
+    component_index = 0
     for nz in n_components_array:
-        quantizer = GaussianIBRateAllocator(R_tot=64)
+        quantizer = GaussianIBRateAllocator(R_tot=args.quantization_bits)
         quantizer.solve_allocation_problem(Xtr,Ytr,nz)
 
         A_matrix = quantizer.optim["AMatrix"]
@@ -70,7 +84,7 @@ def main():
         Y_hat = reg.predict(Z_test)
 
         #Ideal NMSE
-        nmse_db_array[component_index] = compute_nmse(Ytest,Y_hat)
+        nmse_db_array[component_index] = 10*np.log10(compute_nmse(Ytest,Y_hat))
 
         #Quantization and Decoding
         codebooks,thresholds = fit_scalar_lloyd_codebooks(Ztr,BitsPerMode)
@@ -78,13 +92,16 @@ def main():
 
         Y_hat_quantized = reg.predict(Z_q)
 
-        nmse_quantization_array[component_index] = compute_nmse(Ytest,Y_hat_quantized)
+        nmse_quantization_array[component_index] = 10*np.log10(compute_nmse(Ytest,Y_hat_quantized))
 
         component_index+=1
 
-    plt.plot(n_components_array,nmse_db_array,marker='v')
-    plt.plot(n_components_array,nmse_quantization_array,marker='o')
-    plt.grid()
+    set_matplotlib_style(14)
+    plt.plot(n_components_array,nmse_db_array,marker='v',label=r'Ideal')
+    plt.plot(n_components_array,nmse_quantization_array,marker='o',label=fr'$N_b={args.quantization_bits}$ bits')
+    plt.legend()
+    plt.xlabel('Number of Components')
+    plt.ylabel('NMSE [dB]')
     plt.show()
 
 
